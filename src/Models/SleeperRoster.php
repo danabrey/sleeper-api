7681f1837d82f44744829c80dcaaf17d719b5398
danabrey <dan.abrey@kitchencut.com>
2020-07-10 14:33:25 +0100

    Bugfix: unowned team has an empty owner id

--- src/Models/SleeperRoster.php
+++ src/Models/SleeperRoster.php
@@ -4,7 +4,7 @@ namespace DanAbrey\SleeperApi\Models;
 final class SleeperRoster
 {
     public string $roster_id;
-    public string $owner_id;
+    public ?string $owner_id = null;
     /**
      * @var array|string[]
      */
